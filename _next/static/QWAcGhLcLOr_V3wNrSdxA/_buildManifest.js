a57b723b89b57fcc0b315ab352f66b3b71e73304
Glitchtnt <88544006+Glitchtnt@users.noreply.github.com>
2022-04-04 14:49:49 +0800

    Delete _next/static directory

--- _next/static/QWAcGhLcLOr_V3wNrSdxA/_buildManifest.js
+++
@@ -1,7 +0,0 @@
-self.__BUILD_MANIFEST = {
-    __rewrites: [],
-    "/": ["static\u002Fchunks\u002Fpages\u002Findex-27867fe5680169c23b8a.js"],
-    "/_error": ["static\u002Fchunks\u002Fpages\u002F_error-2bc5f6e843d05fd32877.js"],
-    sortedPages: ["\u002F", "\u002F_app", "\u002F_error"]
-};
-self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
